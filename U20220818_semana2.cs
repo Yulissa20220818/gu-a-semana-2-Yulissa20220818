677dd4bfaf9dd678507189f6e6854620eb59f129
Yulissa20220818 <140362195+Yulissa20220818@users.noreply.github.com>
2023-08-07 15:23:19 -0600

    Declaracion de variables semana2

---
+++ U20220818_semana2.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Declaracion de variables 
+
+            //Items 1- Variables string 
+
+            string primerNombre= "Alexandra"; 
+            string segundoNombre= "Yulissa";
+            string primerApellido= "Hernandez";
+            string segundoApellido= "Gaitan";
+
+            //Items 2- Variables integer 
+
+            int miEdad= "19";
+            int edadPapa= "53";
+            int edadMama= "48";
+
+            //Items 3- Variables float 
+
+            float pasajes="15";
+            float comida= "20";
+            float ropa= "30";
+
+            //Items 4- Variables boolean 
+
+            bool estudianteActivo= true;
+            bool estudianteBecado= false;
+
+            //Autoevaluacion: 9.5 
+
+
+
+
+
+        }
+        
+
+
+
+
+    }
+}
